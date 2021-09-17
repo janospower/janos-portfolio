e1e6121f61e98f01dc6e00c342670c615053857d
janospower <pauer.janos@gmail.com>
2021-09-17 12:27:07 +0200

    basic css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,17 @@
 <svelte:head>
 	<title>Hello</title>
 	<meta name="description" content="A description of the page" />
+
+	<meta itemprop="name" content="Content Title" />
+	<meta itemprop="description" content="Content description less than 200 characters" />
+	<meta itemprop="image" content="https://example.com/image.jpg" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+
+	<meta property="og:title" content="Title of the article" />
+	<meta property="og:image" content="//media.example.com/ 1234567.jpg" />
+	<meta property="og:description" content="Description that will show in the preview" />
+	<meta property="og:url" content="//www.example.com/URL of the article" />
 </svelte:head>
 
 <h1>Welcome to SvelteKit</h1>
