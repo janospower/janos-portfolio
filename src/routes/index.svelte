502285c2adc8f6caa603672228dd209a9793a35e
janospower <pauer.janos@gmail.com>
2021-09-17 11:28:25 +0200

    Add some placeholder meta data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+	<title>Hello</title>
+	<meta name="description" content="A description of the page" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
