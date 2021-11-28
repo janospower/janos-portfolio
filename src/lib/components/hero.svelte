3d90e58b7310226aed7dfb0c689ded8844e937fa
Janos <pauer.janos@gmail.com>
2021-11-28 01:35:33 +0100

    add bloom

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -7,17 +7,24 @@
     });
 </script>
 
-<div class="hero" id="hero">
-    <canvas bind:this={el}>
-    <div class="placeholder"></div>
-    </canvas>
+<div class="hero-wrapper">
+    <div class="hero" id="hero">
+        <canvas bind:this={el}>
+        <div class="placeholder"></div>
+        </canvas>
+    </div>
 </div>
 
 <style lang="scss">
-    .hero {
-        width: 80%;
-        margin: 0 auto;
+    .hero-wrapper {
         margin-bottom: var(--spacing-08);
+    }
+    .hero {
+        width: calc(410px + 300px);
+        margin: 0 auto;
+        background: radial-gradient(closest-side, hsla(331, 100%, 70%, 30%) 200px, hsla(310, 100%, 50%, 10%) 215px, hsla(280, 100%, 50%, 0%) 100%);
+        padding: 150px;
+        margin:-150px;
     }
     .placeholder {
         width: 410px;
