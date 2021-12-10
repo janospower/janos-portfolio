e3f3caf27d7481017342fc749dc840375666e3ca
Janos <pauer.janos@gmail.com>
2021-12-10 11:16:49 +0100

    add border

--- src/lib/components/selected-layer.svelte
+++ src/lib/components/selected-layer.svelte
@@ -4,10 +4,10 @@
 
 <style lang="scss">
 	span {
-		/* Fallback: Set a background color. */
+		// display: inline-block;
 		background-color: var(--color-accent--on-dark--teal--opaque);
+		color: var(--color-accent--on-dark--teal--opaque);
 
-		/* Create the gradient. */
 		background-image: var(--gradient-on-dark--volt-teal);
 
 		background-size: 200%;
@@ -17,6 +17,10 @@
 		-webkit-text-fill-color: transparent;
 
 		animation: gradient 10s ease infinite;
+
+		border: 6px solid;
+		border-image: url('/images/selection-border.svg');
+		border-image-slice: 3;
 	}
 
 	@keyframes gradient {
