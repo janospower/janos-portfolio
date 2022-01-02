0b260bf1353af7772f7e6559f21bd4fc95c99861
Janos <pauer.janos@gmail.com>
2022-01-02 20:44:19 +0100

    add social links

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,19 @@
 <footer class="footer">
 	<nav>
 		<a href="/imprint">Imprint</a>
+		<a href="https://de.linkedin.com/in/janospauer" target="_blank">LinkedIn</a>
+		<a href="https://www.figma.com/@janos" target="_blank">Figma</a>
+		<a href="https://github.com/janospower" target="_blank">GitHub</a>
+		<a href="https://www.npmjs.com/~janospower" target="_blank">npm</a>
+		<a href="https://twitter.com/janospauer" target="_blank">twitter</a>
+		<a href="https://medium.com/@Janosp" target="_blank">Medium</a>
 	</nav>
 </footer>
+
+<style lang="scss">
+	footer {
+		padding-top: var(--spacing-10);
+		padding-bottom: var(--spacing-12);
+		background-color: var(--color-neutral--800);
+	}
+</style>
