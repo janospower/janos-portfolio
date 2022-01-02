2157389582d5726260fab61e72d9665e824fea6c
Janos <pauer.janos@gmail.com>
2022-01-02 18:13:32 +0100

    add case highlight color

--- src/lib/components/case.svelte
+++ src/lib/components/case.svelte
@@ -2,7 +2,10 @@
 	export let title = 'Title of Project';
 	export let date = '2022';
 	export let body = 'Body text';
-	export let highlightColor = '0, 0, 0';
+	export let highlightColorHSLA = { h: 0, s: '50%', l: '50%', a: 0.1 };
+	let highlightColor = `${highlightColorHSLA.h}, ${highlightColorHSLA.s}, ${highlightColorHSLA.l}`;
+	let backgroundColor = `${highlightColorHSLA.h}, 100%, 50%, 0.07`;
+	let color = `${highlightColorHSLA.h}, 100%, 10%`;
 	export let tags = [
 		{ name: 'Tag Name 1', certified: false },
 		{ name: 'Tag Name 2', certified: false }
@@ -16,11 +19,11 @@
 			src={imgURL}
 			alt="Case hero"
 			style="filter: 
-            drop-shadow(0px 2.2px 3.74px rgba({highlightColor}, 0.045))
-			drop-shadow(0px 5.56px 9.47px rgba({highlightColor}, 0.0646))
-			drop-shadow(0px 11.34px 19.32px rgba({highlightColor}, 0.08))
-			drop-shadow(0px 23.36px 39.79px rgba({highlightColor}, 0.0954))
-			drop-shadow(0px 64px 109px rgba({highlightColor}, 0.115));"
+            drop-shadow(0px 2.2px 3.74px hsla({highlightColor}, 0.045))
+			drop-shadow(0px 5.56px 9.47px hsla({highlightColor}, 0.0646))
+			drop-shadow(0px 11.34px 19.32px hsla({highlightColor}, 0.08))
+			drop-shadow(0px 23.36px 39.79px hsla({highlightColor}, 0.0954))
+			drop-shadow(0px 64px 109px hsla({highlightColor}, 0.115));"
 		/>
 	</figure>
 	<div class="description">
@@ -29,7 +32,9 @@
 		<p>{body}</p>
 		<div class="tags">
 			{#each tags as { name, certified }, i}
-				<span class="tag">{name}</span>
+				<span class="tag" style="color: hsl({color}); background-color: hsla({backgroundColor})"
+					>{name}</span
+				>
 			{/each}
 		</div>
 	</div>
