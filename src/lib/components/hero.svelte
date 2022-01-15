6395c5527010b187c5c6ad8b32ae53c249e5dffa
Janos <pauer.janos@gmail.com>
2022-01-15 02:43:13 +0100

    parameters for gradient

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,21 +1,45 @@
 <script>
-	let hero;
-	let canvas;
+	let gradients = [
+		{
+			x: 80,
+			y: 0
+		},
+		{
+			x: 0,
+			y: 50
+		},
+		{
+			x: 80,
+			y: 50
+		},
+		{
+			x: 0,
+			y: 100
+		},
+		{
+			x: 80,
+			y: 100
+		},
+		{
+			x: 0,
+			y: 0
+		}
+	];
+
+	let style = `
+			background-image:
+			radial-gradient(at ${gradients[0].x}% ${gradients[0].y}%, var(--color-accent--red--opaque) 0, transparent 50%),
+			radial-gradient(at ${gradients[1].x}% ${gradients[1].y}%, var(--color-accent--magenta--opaque) 0, transparent 50%),
+			radial-gradient(at ${gradients[2].x}% ${gradients[2].y}%, var(--color-accent--blue--opaque) 0, transparent 50%),
+			radial-gradient(at ${gradients[3].x}% ${gradients[3].y}%, var(--color-accent--teal--opaque) 0, transparent 50%),
+			radial-gradient(at ${gradients[4].x}% ${gradients[4].y}%, var(--color-accent--volt--opaque) 0, transparent 50%),
+			radial-gradient(at ${gradients[5].x}% ${gradients[5].y}%, var(--color-accent--yellow--opaque) 0, transparent 50%);
+			`;
 </script>
 
 <div class="hero-wrapper">
-	<div
-		class="hero"
-		id="hero"
-		bind:this={hero}
-		on:pointermove={(e) => {
-			// boundingBox = hero.getBoundingClientRect();
-			// offsetX = (e.clientX - boundingBox.left - boundingBox.width / 2) * 0.01;
-			// offsetY = (e.clientY - boundingBox.top - boundingBox.height / 2) * 0.01;
-			// e.preventDefault();
-		}}
-	>
-		<div class="canvas" bind:this={canvas} />
+	<div class="hero">
+		<div class="canvas" {style} />
 	</div>
 </div>
 
@@ -24,17 +48,6 @@
 		width: 100%;
 		height: 100%;
 		border-radius: 50%;
-		background-image: radial-gradient(
-				at 40% 40%,
-				var(--color-accent--red--opaque) 0,
-				transparent 50%
-			),
-			radial-gradient(at 80% 0%, var(--color-accent--red--opaque) 0, transparent 50%),
-			radial-gradient(at 0% 50%, var(--color-accent--magenta--opaque) 0, transparent 50%),
-			radial-gradient(at 80% 50%, var(--color-accent--blue--opaque) 0, transparent 50%),
-			radial-gradient(at 0% 100%, var(--color-accent--teal--opaque) 0, transparent 50%),
-			radial-gradient(at 80% 100%, var(--color-accent--volt--opaque) 0, transparent 50%),
-			radial-gradient(at 0% 0%, var(--color-accent--yellow--opaque) 0, transparent 50%);
 		background-position: center;
 		background-size: cover;
 		background-repeat: no-repeat;
@@ -46,32 +59,17 @@
 		flex-direction: column;
 	}
 	.hero {
-		width: 100vw;
-		height: 100vw;
+		width: 410px;
+		height: 410px;
 		margin: 0 auto;
-		background-image: radial-gradient(
-			closest-side,
-			hsla(0, 0%, 100%, 100%) 205px,
-			hsla(331, 50%, 90%, 50%) 206px,
-			hsla(290, 80%, 80%, 13%) 210px,
-			hsla(240, 100%, 50%, 7%) 280px,
-			hsla(280, 100%, 50%, 0%) 100%
-		);
-		padding: calc(50vw - 205px);
-		margin: calc((50vw - 205px) * -1);
+		background: white;
+		border-radius: 50%;
 	}
 	@media (max-width: 667px) {
 		.hero {
-			width: 100%;
-			height: 100%;
+			width: calc(100vw - var(--spacing-05) * 2);
+			height: calc(100vw - var(--spacing-05) * 2);
 			border-radius: 50%;
-			background: radial-gradient(
-				closest-side,
-				hsla(0, 0%, 0%, 50%) 90%,
-				hsla(0, 0%, 4%, 50%) 95%,
-				hsla(0, 0%, 20%, 50%) 99%,
-				hsla(0, 0%, 40%, 50%) 100%
-			);
 		}
 	}
 </style>
